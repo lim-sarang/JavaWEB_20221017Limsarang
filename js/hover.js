efbe277ce1863679083d52d92cffb0380c0db445
임사랑 <sarang@sungkyul.ac.kr>
2023-06-13 09:05:58 +0000

    5주차 수정

---
+++ js/hover.js
@@ -0,0 +1,7 @@
+function over(obj) {
+	obj.src="img/google_logo_2.png";
+}
+
+function out(obj) {
+	obj.src="img/google_logo_1.png";
+}
--- js/search.js
+++ js/search.js
@@ -1,5 +1,13 @@
 function search_message(){
-   alert("검색을 수행합니다!"); 
+    let search_str = document.querySelector("#search_txt"); // 변수에 저장
+       if(search_str.value.length === 0){ // 문자 길이, 엄격한 비교
+       alert("검색어가 비었습니다. 입력해주세요"); 
+    }
+    else{
+       alert("검색을 수행합니다!");
+       let text = document.getElementById("search_message").innerHTML = search_str.value;
+       document.querySelector("#form_main").submit();
+    }
 }
 
 document.getElementById("search_btn").addEventListener('click', search_message);
